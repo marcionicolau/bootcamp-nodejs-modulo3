858c212ca36b865b7f6eda8cc755480e41048fa9
Marcio Nicolau <marcionicolau@me.com>
2019-04-30 19:29:45 -0300

    Add Ads CRUD

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,15 @@ const controllers = require('./app/controllers')
 routes.post('/sessions', controllers.SessionController.store)
 routes.post('/users', controllers.UserController.store)
 
-routes.get('/test', authMiddleware, (req, res) => res.json({ ok: true }))
+routes.use(authMiddleware)
+
+/**
+ * Ads
+ */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:ids', controllers.AdController.destroy)
 
 module.exports = routes
